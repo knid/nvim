ef087dca2618c231f2e3ea0dd31860903612f215
Sinan Kanidağlı <sinan_kanidagli@hotmail.com>
2022-12-27 11:52:29 +0300

    null_ls 'eslint_d' to 'eslint'

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -16,7 +16,7 @@ null_ls.setup({
   sources = {
     null_ls.builtins.formatting.prettier,
     -- null_ls.builtins.diagnostics.autopep8,
-    null_ls.builtins.diagnostics.eslint_d.with({
+    null_ls.builtins.diagnostics.eslint.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
     null_ls.builtins.completion.spell,
